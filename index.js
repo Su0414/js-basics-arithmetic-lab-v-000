ef16add7ff86b1748d54309e6e7bb3bfc71c9a0e
Suchitra Naik <susanaik@gmail.com>
2018-07-02 22:01:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 let newID = 1000000000 + oldID;
 let ageIsValid;
+let spyOnNumberIsInteger=0;
 
 if (currentAgeIsInteger) {
   ageIsValid = true;
 } else {
   ageIsValid = false;
 }
+
+spyOnNumberIsInteger.isInteger();
